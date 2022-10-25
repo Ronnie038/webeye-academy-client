c7eac33908c51ce9e88a5bc6c1e89af634751b0f
Israfil <mdronnieronniers@gmial.com>
2022-10-25 22:00:18 +0600

    courses right side nav created

---
+++ src/pages/courses/rightSideNav/RightSideNav.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import CourseSammary from '../courseSummery/CourseSammary';
+
+const RightSideNav = () => {
+	const [courses, setCourses] = useState([]);
+	// console.log(courses);
+
+	useEffect(() => {
+		fetch('http://localhost:5000/category/all')
+			.then((res) => res.json())
+			.then((data) => setCourses(data));
+	}, []);
+	return (
+		<div className='container text-center'>
+			<div className='row '>
+				{courses.map((course) => (
+					<div key={course._id} className='col col-md-6'>
+						<CourseSammary course={course} />
+					</div>
+				))}
+			</div>
+		</div>
+	);
+};
+
+export default RightSideNav;
